ab49a496b6e622a569570df7a7360e4e35f9be7d
nellaccia <47790366+nellaccia@users.noreply.github.com>
2024-05-14 23:34:28 +0100

    sportdb api - correzioni

--- packages/api/client/sportdb.py
+++ packages/api/client/sportdb.py
@@ -4,20 +4,17 @@ from client import Client
 
 class Api(Client):
   def __init__(self, public_api_url=None, tool_api_url=None) -> None:
-
-    if public_api_url is None:
-      public_api_url = "https://thesportsdb.com/api/v1/json/3/"
-
-    self.public_api_url = public_api_url
+    self.url = self.SPORTDB_BASE_URL
+    #"https://thesportsdb.com/api/v1/json/3/"
 
   # Lista leghe italiane
-  # 
-  def playerslist(self, args={}):
-    url = self.public_api_url + "search_all_leagues.php?c=Italy&s=Soccer"
+  # sportdb
+  def leagueslist(self, args={}):
+    url = self.url + self.ENDPOINT_ALL_LEAGUE #"search_all_leagues.php?c=Italy&s=Soccer"
     return self.make_request(url, payload=args)  
   
         #country_name_slug = self.make_slug(args["country_name"])
-        #url += urllib.parse.quote(country_name_slug) + "/"
+        #url += "search_all_leagues.php?c=" + urllib.parse.quote(country_name_slug) + "&s=Soccer/"
 
   def main(args):
     pass
