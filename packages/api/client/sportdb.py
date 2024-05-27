c635078d20cfd78d99482056a4876d72463e4e99
nellaccia <47790366+nellaccia@users.noreply.github.com>
2024-05-27 10:54:10 +0100

    prova sportdb

--- packages/api/client/sportdb.py
+++ packages/api/client/sportdb.py
@@ -1,17 +1,41 @@
+import os
 import urllib.parse
 
 from client import Client
 
 class Api(Client):
-  def __init__(self, public_api_url=None, tool_api_url=None) -> None:
-    self.url = self.SPORTDB_BASE_URL
+  def __init__(self, args, public_api_url=None, tool_api_url=None) -> None:
+    self.url = args.get("SPORTDB_BASE_URL")
+    self.ENDPOINT_ALL_LEAGUE = args.get("ENDPOINT_ALL_LEAGUE")
+    self.ENDPOINT_ALL_EVENTS = args.get("ENDPOINT_ALL_EVENTS")
+    self.ENDPOINT_SEARCH_TEAM = args.get("ENDPOINT_SEARCH_TEAM")
+    self.API_PROVA = args.get("API_PROVA")
     #"https://thesportsdb.com/api/v1/json/3/"
 
   # Lista leghe italiane
   # sportdb
+  #https://nuvolaris.dev/api/v1/web/tmarinelli/api/client?module=sportdb&action=leagueslist
   def leagueslist(self, args={}):
     url = self.url + self.ENDPOINT_ALL_LEAGUE #"search_all_leagues.php?c=Italy&s=Soccer"
     return self.make_request(url, payload=args)  
+  
+        #country_name_slug = self.make_slug(args["country_name"])
+        #url += "search_all_leagues.php?c=" + urllib.parse.quote(country_name_slug) + "&s=Soccer/"
+
+  def events(self, args={}):
+    url = self.url + self.ENDPOINT_ALL_EVENTS 
+    return self.make_request(url, payload=args)
+  
+  def searchTeam(self, args={}):
+    url = self.url + self.ENDPOINT_SEARCH_TEAM 
+    return self.make_request(url, payload=args)
+
+  def prova(self):
+      return "pippo"  
+
+  def prova2(self):
+    url = self.API_PROVA
+    return self.make_request(url)
   
         #country_name_slug = self.make_slug(args["country_name"])
         #url += "search_all_leagues.php?c=" + urllib.parse.quote(country_name_slug) + "&s=Soccer/"
