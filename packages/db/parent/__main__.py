31b5bef099d1836e49a8c54a4e7d203a7d173bb8
Antonio Piga <antonio.piga93@gmail.com>
2024-06-02 18:33:35 +0100

    feat: db design

---
+++ packages/db/parent/__main__.py
@@ -0,0 +1,32 @@
+#--web true
+#--kind python:default
+#--param POSTGRES_URL $POSTGRES_URL
+from dataclasses import dataclass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.article import Article
+
+def main(args):
+   
+    db = DbConnection()
+    db.connect_to_db(args.get("POSTGRES_URL"))
+    if(args.get("model") == "article"):
+        return Article.handler(db.engine, args)
+
+    return { 
+        "body": "hello world!"
+    }
+
+@dataclass
+class DbConnection:
+    engine = None
+    session = None
+
+    def connect_to_db(self, db_url):
+        try:
+            self.engine = create_engine(db_url)
+            Session = sessionmaker(bind=self.engine)
+            self.session = Session()
+            print("Connession to db ok.")
+        except Exception as e:
+            print("Erroreduring db conn:", e)
