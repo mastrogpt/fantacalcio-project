b3e47d362cb81a3512ab3511f71e665cf77fdd52
Antonio Piga <antonio.piga93@gmail.com>
2024-07-27 16:33:53 +0100

    feat: add chat to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from '$lib/components/molecules/Header.svelte';
 	import Footer from '$lib/components/molecules/footer/footer.svelte';
+	import Widget from '$lib/components/widget/Widget.svelte';
 	import consoleMessage from '$lib/utils/consoleMessage';
 
 	import '../app.css';
@@ -10,6 +11,7 @@
 
 <main>
 	<slot />
+	<Widget />
 </main>
 
 <Footer />
