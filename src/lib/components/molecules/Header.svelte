3dcf4d5f1a6df98606a9bcdfa3f8692723bb9a48
Antonio Piga <antonio.piga93@gmail.com>
2024-07-28 17:40:11 +0100

    fix: onclick rollback on hero slider slide

--- src/lib/components/molecules/Header.svelte
+++ src/lib/components/molecules/Header.svelte
@@ -1,6 +1,10 @@
 <script>
 	import { page } from '$app/stores';
 	import Navbar from './navbar/navbar.svelte';
+
+	$: route = $page.route.id;
+
+	let showMenu = false;
 </script>
 
 <header class="bg-primary">
