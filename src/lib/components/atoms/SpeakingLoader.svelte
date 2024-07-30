ec6f380a1fb295a8d2b5f77f1df5165c05327028
Antonio Piga <antonio.piga93@gmail.com>
2024-07-30 17:22:16 +0100

    feat: start speaking loader

---
+++ src/lib/components/atoms/SpeakingLoader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let variant: 'primary' | 'accent' = 'primary';
+
+	let message = 'un attimo...';
+
+	setTimeout(() => {
+		message = 'ci sto pensando';
+	}, 3000);
+</script>
+
+<div class="flex flex-col justify-center items-center h-30 p-10">
+	<div class={`animate-spin rounded-full h-10 w-10 border-b-2 border-${variant}`}></div>
+	<p class="mt-4 text-${variant}">{message}</p>
+</div>
+
