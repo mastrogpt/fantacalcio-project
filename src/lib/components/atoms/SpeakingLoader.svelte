db3fc9509b725778f65c32b5325dc8f26f789b18
Antonio Piga <antonio.piga93@gmail.com>
2024-08-08 15:34:30 +0100

    refactor: loader speaking

--- src/lib/components/atoms/SpeakingLoader.svelte
+++ src/lib/components/atoms/SpeakingLoader.svelte
@@ -1,32 +1,39 @@
 <script lang="ts">
-	export let variant: 'primary' | 'accent' = 'primary';
+	export let variant: 'primary' | 'accent' = 'accent';
 
-	let message = 'Un attimo';
+	let message = 'Mo se vede';
 
 	setTimeout(() => {
-		message = "Mo' t'o dico";
-	}, 3000);
+		message = 'Stamo a lavora';
+		setTimeout(() => {
+			message = "Aoh, ce vo' tempo...";
+		}, 9000);
+	}, 4000);
 </script>
 
 <div class="flex flex-col justify-center items-center h-30 p-10">
-	<p class={`mt-4 text-${variant} ${message === 'Un attimo' ? 'ellipsis' : ''}`}>
-		{message}<span class="dots"></span>
+	<p class={`mt-4 text-${variant}`}>
+		<strong>{message}</strong>
+		<span class="dots"></span>
 	</p>
 </div>
 
 <style>
 	@keyframes ellipsis {
 		0% {
-			content: '.';
+			visibility: visible;
 		}
-		33% {
-			content: '..';
+		25% {
+			visibility: hidden;
 		}
-		66% {
-			content: '...';
+		50% {
+			visibility: visible;
+		}
+		75% {
+			visibility: hidden;
 		}
 		100% {
-			content: '.';
+			visibility: visible;
 		}
 	}
 
@@ -40,18 +47,21 @@
 		}
 	}
 
+	.dots {
+		display: inline-block;
+		animation: bounce 0.6s infinite;
+	}
+
 	.dots::after {
 		content: '.';
-		animation:
-			ellipsis 1.5s steps(3, end) infinite,
-			bounce 0.6s infinite;
+		animation: ellipsis 1.5s infinite;
 	}
 
 	.text-primary {
-		color: #10987d;
+		color: #041c2c;
 	}
 
 	.text-accent {
-		color: #ffd700;
+		color: #10987d;
 	}
 </style>
