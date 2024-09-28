26b2be47baae5be63ae4fecea17642c13f6298d4
Antonio Piga <antonio.piga93@gmail.com>
2024-09-28 16:02:05 +0100

    clean: delete fantamaster code ref

--- src/routes/stats/[playerId]/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getStatsDataById } from '$lib/service/fantamaster/getStats';
-
-export function load({ params }: { params: { playerId: number } }) {
-    
-    return getStatsDataById(params.playerId).then((result) => {
-        const playerData = result;
-        return { playerData };
-    });
-}
