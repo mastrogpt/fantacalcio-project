4deb8680f359eb131383cea5d20554a73d1616e6
Antonio Piga <antonio.piga93@gmail.com>
2024-10-07 17:21:02 +0100

    fix: add nuv logo

--- src/lib/components/atoms/sliders/HeroSlider.svelte
+++ src/lib/components/atoms/sliders/HeroSlider.svelte
@@ -14,9 +14,9 @@
 
 	const data = [
 		{
-			title: '<h2> FantaBalùn  &#x1F916 </h2> <h6>er Bot che ne sa un Bot-to </h6> ',
+			title: '<h2> FantaBalùn  &#x1F916  </h2> <h5> Powered by Nuvolaris</h5>   ',
 			description:
-				'<p class="sm:text-sm text-base md:text-lg">Chiedi <strong> ciò che vuoi</strong>, come ad un amico al bar. Pareri, confronti tra calciatori. <strong>Allega lo screen </strong> di un messaggio in cui ti si propone uno scambio: BalùnBot ti dirà se conviene. No, <strong>non scola la pasta!</strong> </p>',
+				'<h5>Er Bot che ne sa un Bot-to !</h5> <p class="sm:text-sm text-base md:text-lg">Chiedi <strong> ciò che vuoi</strong>, come ad un amico al bar. Pareri, confronti tra calciatori. <strong>Allega lo screen </strong> di un messaggio in cui ti si propone uno scambio: BalùnBot ti dirà se conviene. No, <strong>non scola la pasta!</strong> </p>',
 			imageUrl: logoBalunBg,
 			buttonLabel: 'Chiedilo al bot',
 			buttonOnClick: () => handleNuvBotChatOpening(true),
